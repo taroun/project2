0efa940cb5544b7b0ddb647993ab5f7c9cf0d428
taroun <aksghk4671@gmail.com>
2021-11-02 15:17:49 +0900

    11/02 test 02

--- djangoProject2/settings.py
+++ djangoProject2/settings.py
@@ -20,7 +20,21 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-0bl!h!(&87)%s41&)9p1#w4iirv#qig48o25k0y48)&--n1t!^'
+env_list = dict()
+
+local_env = open(os.path.join(BASE_DIR, '.env'))
+
+while True:
+    line = local_env.readline()
+    if not line:
+        break
+    line = line.replace('\n', '')
+    start = line.find('=')
+    key = line[:start]
+    value = line[start+1:]
+    env_list[key] = value
+
+SECRET_KEY = env_list['SECRET_KEY']
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
